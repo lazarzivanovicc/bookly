7735636879ecb0f5a22d37cce348cd20e9cf87cb
lazarzivanovicc <lazar.zivanovic11@gmail.com>
2024-11-24 21:28:47 +0100

    adding deps

--- project.clj
+++ project.clj
@@ -7,7 +7,11 @@
                  [ring/ring-defaults "0.3.2"]
                  [midje "1.10.9"]
                  [org.clojure/data.csv "1.1.0"]
-                 [ring/ring-json "0.3.1"]]
+                 [ring/ring-json "0.3.1"]
+                 [buddy/buddy-auth "3.0.1"]
+                 [com.github.seancorfield/next.jdbc "1.3.955"]
+                 [org.postgresql/postgresql "42.7.4"]
+                 [com.github.seancorfield/honeysql "2.6.1230"]]
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler bookly.core/app}
   :profiles
